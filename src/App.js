24a0827dfe152bb0abf52abab7daafe269c29f47
MariaJoseDiazCa <mariajose.diazca@gmail.com>
2022-01-13 13:41:29 -0300

    ADD: call to API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
+import { useState } from "react";
 import { Formik, Form, Field } from "formik";
 import "./header.css";
 
 const App = () => {
+  const [photos, setPhotos] = useState([]);
+  console.log(photos);
+
   return (
-    <div>
+    <>
       <header>
         <Formik
           initialValues={{ search: "" }}
           onSubmit={async (values) => {
-            //
-            console.log(values);
+            const response = await fetch(
+              `https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
+              {
+                headers: {
+                  Authorization:
+                    "Client-ID qvy2y7fRCE_mIZrDJ8fevnKO8Mm29ZYhiXQSRq70mOw",
+                },
+              }
+            );
+            const data = await response.json();
+            setPhotos(data.results);
           }}
         >
           <Form>
-            <Field name="Search" />
+            <Field name="search" />
           </Form>
         </Formik>
       </header>
-    </div>
+    </>
   );
 };
 
